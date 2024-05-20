bbc88468d6ed03e2250e5ace856f208c88ecfd20
Nakano San <antoinef30350@gmail.com>
2024-05-20 10:13:30 +0000

    Initial commit with basic React setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Map from './components/Map';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>RDR2 Legendary Animals Map</h1>
+      <Map />
+    </div>
+  );
+};
+
+export default App;
